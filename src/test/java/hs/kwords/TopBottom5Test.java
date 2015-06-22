0a2d4024920b785ed0871d279383ee29f77b8c8d
Saleem Ansari <tuxdna@gmail.com>
2015-06-22 12:03:23 +0530

    Fix hard-coded path of stop_words.txt

--- src/test/java/hs/kwords/TopBottom5Test.java
+++ src/test/java/hs/kwords/TopBottom5Test.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 import junit.framework.TestCase;
 
 public class TopBottom5Test extends TestCase {
+	String stopWordsFilePath = "stop_words.txt";
+
 	@Test
 	public void testTokenizer() throws Exception {
 		String myString = "This is an amazing world!";
@@ -32,7 +34,6 @@ public class TopBottom5Test extends TestCase {
 
 	@Test
 	public void testStopWordsFilter() throws Exception {
-		String stopWordsFilePath = "/home/tuxdna/work/helpshift/stop_words.txt";
 		StopWordFilter filter = new StopWordFilter(stopWordsFilePath);
 		assertNull(filter.filter("is"));
 		assertNull(filter.filter("an"));
@@ -44,7 +45,6 @@ public class TopBottom5Test extends TestCase {
 		String myString = "This is an amazing world!";
 		InputStream is = new ByteArrayInputStream(myString.getBytes());
 		Tokenizer tokenizer = new EnglishTokenizer(is);
-		String stopWordsFilePath = "/home/tuxdna/work/helpshift/stop_words.txt";
 		StopWordFilter stopWordFilter = new StopWordFilter(stopWordsFilePath);
 		LowerCaseFilter lCaseFilter = new LowerCaseFilter();
 		TokenStream ts = new TokenStream(tokenizer, lCaseFilter, stopWordFilter);
